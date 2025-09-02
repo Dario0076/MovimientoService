c81c873df419ee7855e6653ef75e841434fd2b27
Dario0076 <0943605360@aitec.edu.ec>
2025-09-02 15:42:56 -0500

    Agregar configuración CORS para permitir solicitudes desde cualquier origen

--- src/main/java/com/inventario/MovimientoService/controller/MovimientoController.java
+++ src/main/java/com/inventario/MovimientoService/controller/MovimientoController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/movimientos")
+@CrossOrigin("*")
 public class MovimientoController {
     private final MovimientoService movimientoService;
 
